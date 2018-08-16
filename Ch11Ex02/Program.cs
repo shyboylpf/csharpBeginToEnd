c78efb5c21a1b1856c0324f77a2fcbebe9aeff8d
刘朋芳 <lpf_mart@163.com>
2018-08-16 17:50:30 +0800

    反射 MemberInfo

---
+++ Ch11Ex02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Ch11Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nReflection.MemberInfo");
+            Type MyType = Type.GetType("System.IO.File");
+            MemberInfo[] Mymemberinfoarray = MyType.GetMembers();
+            Console.WriteLine("\nThere are {0} members in {1}.",
+                Mymemberinfoarray.Length, MyType.FullName);
+            Console.WriteLine("{0}.", MyType.FullName);
+            if (MyType.IsPublic)
+            {
+                Console.WriteLine("{0} is public.", MyType.FullName);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
