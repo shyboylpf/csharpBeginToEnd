cdb82cd82bb7b8766d2f387f07cbea1434308e88
刘朋芳 <lpf_mart@163.com>
2018-08-17 17:37:23 +0800

    启动进程

---
+++ ProcessAndThread/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace ProcessAndThread
+{
+    class Program
+    {
+        void OpenApplication(string myDocumentPath)
+        {
+            Process.Start(myDocumentPath);
+        }
+        void OpenWithArguments()
+        {
+            Process.Start("IExplore.exe", "www.163.com");
+            Process.Start("IExplore.exe", "I:\\temp\\a.html");
+            Process.Start("IExplore.exe", "I:\\temp\\b.jpg");
+        }
+        static void Main(string[] args)
+        {
+            string myDocumentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            Program program = new Program();
+            program.OpenApplication(myDocumentPath);
+            program.OpenWithArguments();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
