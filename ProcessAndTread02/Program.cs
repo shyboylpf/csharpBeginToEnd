b29047fddb1c0ed49f390f61daac1e411ffe0cd0
刘朋芳 <lpf_mart@163.com>
2018-08-20 11:02:02 +0800

    进程线程02

---
+++ ProcessAndTread02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Threading;
+
+namespace ProcessAndTread02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process myProcess;
+                myProcess = Process.Start("Notepad.exe");
+                for(int i=0; i<5; i++)
+                {
+                    if (!myProcess.HasExited)
+                    {
+                        myProcess.Refresh();
+                        Console.WriteLine($"当前资源占用 : {myProcess.WorkingSet64.ToString()}");
+                        Thread.Sleep(2000);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                myProcess.CloseMainWindow();
+                myProcess.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
