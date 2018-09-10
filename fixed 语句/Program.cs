5ffa145d557b5a8b2dff6670d258c6b06334f37a
刘朋芳 <lpf_mart@163.com>
2018-09-10 17:34:42 +0800

    ref

---
+++ fixed 语句/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fixed_语句
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        unsafe private static void ModifyFixedStorage()
+        {
+            Point pt = new Point();
+            fixed(int* p = &pt.x)
+            {
+                *p = 1;
+            }
+        }
+    }
+    class Point
+    {
+        public int x;
+        public int y;
+    }
+
+}
---
+++ ref/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @ref
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModifyProductsByReference();
+        }
+        private static void ModifyProductsByReference()
+        {
+            // Declare an instance of Product and display its initial values.
+            Product item = new Product("Fasteners", 54321);
+            System.Console.WriteLine("Original values in Main.  Name: {0}, ID: {1}\n",
+                item.ItemName, item.ItemID);
+
+            // Pass the product instance to ChangeByReference.
+            ChangeByReference(ref item);
+            System.Console.WriteLine("Back in Main.  Name: {0}, ID: {1}\n",
+                item.ItemName, item.ItemID);
+        }
+
+        private static void ChangeByReference(ref Product itemRef)
+        {
+            // Change the address that is stored in the itemRef parameter.   
+            itemRef = new Product("Stapler", 99999)
+            {
+
+                // You can change the value of one of the properties of
+                // itemRef. The change happens to item in Main as well.
+                ItemID = 12345
+            };
+        }
+    }
+    class Product
+    {
+        public Product(string name, int newID)
+        {
+            ItemName = name;
+            ItemID = newID;
+        }
+        public string ItemName { get; set; }
+        public int ItemID { get; set; }
+    }
+}
