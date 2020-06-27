a986d0ced250c824047d856b6310651a4865f931
Pengfang Liu <lpf_mart@163.com>
2020-06-27 22:38:10 +0800

    一个用yield的优雅斐波那契数列

---
+++ FibonacciGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hello
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var generator = new FibonacciGenerator();
+            foreach (var digit in generator.Generate(20))
+            {
+                Console.WriteLine(digit);
+            }
+        }
+    }
+}
