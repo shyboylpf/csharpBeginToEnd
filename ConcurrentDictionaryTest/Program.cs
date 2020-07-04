c4cfe6b56adb428215908db132cf911bf636e471
Pengfang Liu <lpf_mart@163.com>
2020-07-04 15:33:48 +0800

    Enum / Enum Flag

---
+++ ConcurrentDictionaryTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ConcurrentDictionaryTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ConcurrentDictionary<int, int> concurrentDictionary = new ConcurrentDictionary<int, int>();
+
+            concurrentDictionary.GetOrAdd(1, 1);
+            Console.WriteLine(concurrentDictionary.GetOrAdd(1, 2));
+
+            int tmp = 4;
+            int result = concurrentDictionary.AddOrUpdate(1, tmp, (key, value) => { return tmp; });
+
+            Console.WriteLine(result);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EnumTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnumTest
+{
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Season a = Season.Autumn;
+            Console.WriteLine($"Integral value of {a} is {(int)a}");  // output: Integral value of Autumn is 2
+
+            var b = (Season)1;
+            Console.WriteLine(b);  // output: Summer
+
+            var c = (Season)4;
+            Console.WriteLine(c);  // output: 4
+        }
+    }
+}
