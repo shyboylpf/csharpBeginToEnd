6475ea6c34426617a18f49b61cde2aed0354950a
Pengfang Liu <lpf_mart@163.com>
2020-07-09 15:04:08 +0800

    serilog 的日志等级测试

---
+++ SerilogLevelTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Serilog;
+
+namespace SerilogLevelTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            // 从上到下, 日志等级递增
+            Log.Verbose("Verbose");
+            Log.Debug("debug");
+            Log.Information("Information");
+            Log.Error("Error");
+            Log.Fatal("Fatal");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
