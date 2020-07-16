8a6a956b77648616ce6f8ba8e6ad0fcf4e610d44
Pengfang Liu <lpf_mart@163.com>
2020-07-16 10:11:22 +0800

    .netcore json配置文件

---
+++ JsonConfigrueTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonConfigrueTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
