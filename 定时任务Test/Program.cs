faf827a0ac041ffe9087d1a65e75b8def4302871
Pengfang Liu <lpf_mart@163.com>
2020-07-16 10:11:34 +0800

    定时任务插件

---
+++ 定时任务Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 定时任务Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
