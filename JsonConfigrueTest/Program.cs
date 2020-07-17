7a17a7d150d6d82165e53e26d199247550637a4f
Pengfang Liu <lpf_mart@163.com>
2020-07-17 18:55:52 +0800

    json配置文件

--- JsonConfigrueTest/Program.cs
+++ JsonConfigrueTest/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.IO;
 
 namespace JsonConfigrueTest
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static string HIKDirectory;
+        private static string StoragePath;
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+            HIKDirectory = config["HIKMotionDetectionService:HIKDirectory"]; // 配置键
+            StoragePath = config["HIKMotionDetectionService:StoragePath"]; // 配置键
+            if (!Directory.Exists(HIKDirectory))
+            {
+                Directory.CreateDirectory(HIKDirectory);
+            }
+
+            string desFile = DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg";
+            string fullPath = Path.Combine(StoragePath, desFile);
+            using (FileStream fileStream1 = new FileStream(fullPath, FileMode.OpenOrCreate))
+            {
+                using BinaryWriter bw = new BinaryWriter(fileStream1);
+                bw.Write(new byte[] { 255, 255, 255 });
+                bw.Flush();
+                bw.Close();
+            }
+            FileInfo fileInfo = new FileInfo(fullPath);
+            var fullname = fileInfo.FullName;
+            var name = fileInfo.Name;
+            var length = fileInfo.Length;
+            var datetime = fileInfo.CreationTime;
+
+            Console.WriteLine();
+            Console.WriteLine(HIKDirectory);
+            Console.WriteLine(StoragePath);
         }
     }
 }
