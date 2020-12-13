df2643e7f02db047e470dd3cf7bacc52dbcea982
Pengfang Liu <lpf_mart@163.com>
2020-12-13 21:29:46 +0800

    ocelot网关

---
+++ ocelotTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace ocelotTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new WebHostBuilder()
+               .UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   config
+                       .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                       .AddJsonFile("appsettings.json", true, true)
+                       .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                       .AddJsonFile("ocelot.json")
+                       .AddEnvironmentVariables();
+               })
+               .ConfigureServices(s =>
+               {
+                   s.AddOcelot();
+               })
+               .ConfigureLogging((hostingContext, logging) =>
+               {
+                   //add your logging
+               })
+               .UseIISIntegration()
+               .Configure(app =>
+               {
+                   app.Map("/swagger/v1/swagger.json", b =>
+                   {
+                       b.Run(async x =>
+                       {
+                           var json = File.ReadAllText("swagger.json");
+                           await x.Response.WriteAsync(json);
+                       });
+                   });
+                   app.UseSwaggerUI(c =>
+                   {
+                       c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ocelot");
+                   });
+
+                   app.UseOcelot().Wait();
+               })
+               .Build()
+               .Run();
+        }
+    }
+}
