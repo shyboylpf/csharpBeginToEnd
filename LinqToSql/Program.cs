9ab4985ce59f9789bdf594908d66cb278c0edb64
matt liu <mliu@xignite.com>
2021-07-06 10:17:53 +0800

    linq

---
+++ LinqToSql/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqToSql
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Specify the data source.
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+            // define the query expression.
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            foreach (int i in scoreQuery)
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
+    }
+}
