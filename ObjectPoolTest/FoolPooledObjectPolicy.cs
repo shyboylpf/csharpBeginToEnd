8520793e0480a1807cf350248002e53f1b5932ba
matt liu <mliu@xignite.com>
2021-08-05 18:29:15 +0800

    object pool

---
+++ ObjectPoolTest/FoolPooledObjectPolicy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.ObjectPool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPoolTest
+{
+    internal class FoolPooledObjectPolicy : IPooledObjectPolicy<Foo>
+    {
+        public Foo Create()
+        {
+            return new Foo();
+        }
+
+        public bool Return(Foo obj)
+        {
+            return true;
+        }
+    }
+}
