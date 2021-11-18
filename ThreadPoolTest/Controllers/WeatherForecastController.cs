48552532228d2a03cf314f983e74a885e49f681c
matt liu <mliu@xignite.com>
2021-11-18 21:55:08 +0800

    thread pool test.

---
+++ ThreadPoolTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    //await Task.Delay(500);
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpGet]
+        public async Task<int> Get()
+        {
+            await Task.Delay(500);
+            //ThreadPool.GetMinThreads(out var i, out var j);
+            //return i;
+
+            var i = Thread.CurrentThread.ManagedThreadId;
+            _logger.LogInformation(i.ToString());
+            return 1;
+        }
+
+        //[HttpGet]
+        //public int Get()
+        //{
+        //    return 1;
+        //}
+    }
+}
